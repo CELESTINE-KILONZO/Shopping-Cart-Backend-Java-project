5b2a351e97b667561c6fbf3c478963920653cb50
celly <ckavataki@gmail.com>
2025-12-02 07:57:04 +0300

    added neccessary fields in the  product,image and category tables

--- src/main/java/com/cellythebackenddeveloper/shopping_cart/model/Category.java
+++ src/main/java/com/cellythebackenddeveloper/shopping_cart/model/Category.java
@@ -1,4 +1,23 @@
 package com.cellythebackenddeveloper.shopping_cart.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity( name = "categories")
 
 public class Category {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long  id;
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
