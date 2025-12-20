88aae03f0a6a04197e727746b61b076bf6023573
celly <ckavataki@gmail.com>
2025-12-20 07:13:00 +0300

    created update product endpoint

--- src/main/java/com/cellythebackenddeveloper/shopping_cart/controller/ProductController.java
+++ src/main/java/com/cellythebackenddeveloper/shopping_cart/controller/ProductController.java
@@ -2,12 +2,14 @@ package com.cellythebackenddeveloper.shopping_cart.controller;
 
 import com.cellythebackenddeveloper.shopping_cart.model.Product;
 import com.cellythebackenddeveloper.shopping_cart.request.AddProductRequest;
+import com.cellythebackenddeveloper.shopping_cart.request.ProductUpdateRequest;
 import com.cellythebackenddeveloper.shopping_cart.response.ApiResponse;
 import com.cellythebackenddeveloper.shopping_cart.service.product.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -120,4 +122,13 @@ public class ProductController {
         }
     }
 
+    @PutMapping("/updateproduct")
+    public ResponseEntity<ApiResponse> updateProduct(Long id, ProductUpdateRequest productUpdateRequest) {
+        try {
+            Product updatedProduct = productService.updateProduct(productUpdateRequest, id);
+            return ResponseEntity.ok(new ApiResponse("Product updated successfully", updatedProduct));
+        } catch (Exception e) {
+            return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Failed to update product: ", null));
+        }
+    }
 }
