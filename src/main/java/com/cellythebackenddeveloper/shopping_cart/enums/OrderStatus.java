33fa15f3619e4a0b1dc3bdd2f6d8bb43ea7face4
celly <ckavataki@gmail.com>
2026-02-11 12:08:53 +0300

    created the order and orderItem models with the relevant relationships

---
+++ src/main/java/com/cellythebackenddeveloper/shopping_cart/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.cellythebackenddeveloper.shopping_cart.enums;
+
+public enum OrderStatus {
+    PENDING,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
---
+++ src/main/java/com/cellythebackenddeveloper/shopping_cart/model/Order.java
@@ -0,0 +1,30 @@
+package com.cellythebackenddeveloper.shopping_cart.model;
+import com.cellythebackenddeveloper.shopping_cart.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate orderDate;
+    private BigDecimal totalAmount;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    @OneToMany(mappedBy = "order" , cascade = CascadeType.ALL , orphanRemoval = true)
+    private Set<OrderItem> orderItems = new HashSet<>();
+
+}
