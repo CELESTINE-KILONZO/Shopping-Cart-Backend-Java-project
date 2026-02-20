42c32e25471b83450ff1db7d7af8986fce63b9b5
celly <ckavataki@gmail.com>
2026-02-20 23:47:30 +0300

    started workimg on user controller

--- src/main/java/com/cellythebackenddeveloper/shopping_cart/controller/UserController.java
+++ src/main/java/com/cellythebackenddeveloper/shopping_cart/controller/UserController.java
@@ -1,4 +1,10 @@
 package com.cellythebackenddeveloper.shopping_cart.controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/carts")
 public class UserController {
 }
