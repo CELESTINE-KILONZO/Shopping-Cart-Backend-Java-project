8d0dbd5be7c9dbe1d6d9025b3468c86cd8eeb02e
celly <ckavataki@gmail.com>
2026-02-21 07:01:07 +0300

    created getUserByIdEndpoint

--- src/main/java/com/cellythebackenddeveloper/shopping_cart/controller/UserController.java
+++ src/main/java/com/cellythebackenddeveloper/shopping_cart/controller/UserController.java
@@ -1,10 +1,27 @@
 package com.cellythebackenddeveloper.shopping_cart.controller;
+import com.cellythebackenddeveloper.shopping_cart.response.ApiResponse;
+import com.cellythebackenddeveloper.shopping_cart.service.User.IUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("${api.prefix}/carts")
+@RequestMapping("${api.prefix}/users")
+
 public class UserController {
+    private final IUserService userService;
+    @GetMapping("/{userId}/getUserById")
+    public ResponseEntity <ApiResponse> getUserById(@PathVariable Long userId) {
+        try {
+            return ResponseEntity.ok(new ApiResponse("User retrieved successfully", userService.getUserById(userId)));
+        } catch (Exception e) {
+            return ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 }
