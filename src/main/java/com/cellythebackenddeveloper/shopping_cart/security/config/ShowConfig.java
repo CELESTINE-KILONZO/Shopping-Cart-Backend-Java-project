a43b232c93c61f7b4750e2c1162638e14e357170
CELESTINE-KILONZO <ckavataki@gmail.com>
2026-05-07 09:15:15 +0300

    created the jwt utils

--- src/main/java/com/cellythebackenddeveloper/shopping_cart/security/config/ShowConfig.java
+++ src/main/java/com/cellythebackenddeveloper/shopping_cart/security/config/ShowConfig.java
@@ -1,4 +1,4 @@
-package com.cellythebackenddeveloper.shopping_cart.config;
+package com.cellythebackenddeveloper.shopping_cart.security.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
