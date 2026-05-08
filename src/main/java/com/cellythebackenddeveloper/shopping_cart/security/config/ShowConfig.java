b771afe06f3ef6b3783ea4213a3ed751a0fad948
CELESTINE-KILONZO <ckavataki@gmail.com>
2026-05-08 06:50:33 +0300

    feat: configure Spring Security with JWT authentication

--- src/main/java/com/cellythebackenddeveloper/shopping_cart/security/config/ShowConfig.java
+++ src/main/java/com/cellythebackenddeveloper/shopping_cart/security/config/ShowConfig.java
@@ -1,13 +1,78 @@
 package com.cellythebackenddeveloper.shopping_cart.security.config;
+
+import com.cellythebackenddeveloper.shopping_cart.security.jwt.AuthTokenFilter;
+import com.cellythebackenddeveloper.shopping_cart.security.jwt.JwtAuthEntryPoint;
+import com.cellythebackenddeveloper.shopping_cart.security.user.ShopUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
 @Configuration
+
 public class ShowConfig {
+    private final ShopUserDetailsService shopUserDetailsService;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+
+    private static final List <String> SECURED_URLS = List.of("");
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthTokenFilter authTokenFilter() {
+        return new AuthTokenFilter();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(shopUserDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(SECURED_URLS.toArray(new String[0])).authenticated().anyRequest().permitAll());
+
+        http.authenticationProvider(daoAuthenticationProvider());
+        http.addFilterBefore(authTokenFilter(),
+                UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
 }
